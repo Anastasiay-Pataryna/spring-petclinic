34885b0d98e014c638de5d7c19ddecfad412f169
Anastasiya Pataryna <Anastasiya.Pataryna@gmail.com>
2019-10-29 00:38:14 +0300

    Update ClinicServiceTests.java

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
@@ -214,5 +214,10 @@ class ClinicServiceTests {
         assertThat(visitArr[0].getDate()).isNotNull();
         assertThat(visitArr[0].getPetId()).isEqualTo(7);
     }
+    
+    @Test
+    void pullTest(){
+    System.out.println("I pull changes")
+    }
 
 }
